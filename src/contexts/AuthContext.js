56b8d78da44ed38c630b99607ce7c010d18dbb12
Laura <laura.p.amaral@aluno.senai.br>
2025-06-17 16:50:47 -0300

    Atualizando

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,32 @@
+// importar funções do React necessãrias para criar e usar contexto
+
+import { createContext, useState, useContext } from "react";
+import secureLocalStorage from "react-secure-storage";
+
+//Cria o contexto de autenticacao, que vai permitir compartilhar dados entre componentes
+const AuthContext = createContext();
+
+//Esse componente vai envolver a aplicação (ou parte dela) e fornecer os dados de autenticação para os filhos
+//provider = prover/dar
+export const AuthProvider = ({ children }) => {
+
+    const [usuario, setUsuario] = useState(() => {
+        const usuarioSalvo = secureLocalStorage.getItem("tokenLogin");
+        return usuarioSalvo ? JSON.parse(usuarioSalvo) : undefined;
+    });
+
+
+
+    return (
+        // O AuthContext.Provider permite que qualquer componente dentro dele acesse o 'usuario' e 'setUsuario'
+        //Faz com que qualquer componente que esteja dentro a <AuthProvider> consiga acessar o valor {usuario, set Usuario} usando o hook useAuth()
+        <AuthContext.Provider value={{ usuario, setUsuario }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+//Esse hook personalizado facilita o acesso ao contexto dentro de qualquer componente
+//Usar
+
+export const useAuth = () => useContext(AuthContext);
